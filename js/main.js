b37a4c76a80fac6cd83f427c9982e7a6cbe1cc3e
Samuel Clements <sdkclements@gmail.com>
2017-01-31 12:32:24 -0500

    have basic front view and JS setup

--- js/main.js
+++ js/main.js
@@ -9,6 +9,11 @@ var posts = {
     dist: []
 
 }
+// function createBoard(){
+// 	var pole = $(`<div class='pole'> pole One </div>`)
+// 	$('.mainContainer').append
+
+// }
 
 
 class Disk {
@@ -16,9 +21,11 @@ class Disk {
         this.weight = weight;
         this.post = post;
         this.position = position;
-        // this.element = $("<div> div test</div>");
-        // $("body").append ( this.element );
-        // this.element.addEventListener('click', this, false)
+        this.element = $(`<div class='disk' data-weight=${weight}> Disk: ${weight}</div>`);
+        $('#pol1').append ( this.element );
+        // this.addEventListener('click', function(e) {
+        // 	console.log(e)
+        // })
 
         // Disks.protoype.handleEvent = function(e) {
 // 	switch (e.type) {
@@ -32,19 +39,28 @@ class Disk {
 function createNumDisks(num) {
 
     for (var i = -1; i = num; i++) {
-        posts['source'].push(new Disk(num, 1, num))
-
+    	var disk = new Disk(num, 1, num)
+        posts['source'].push(disk)
         num--
     }
+    $('.disk').on('click', function(e) {
+    	console.log(this)
+    	$(this).toggleClass('activeDisk')
+    })
     //console.log(posts['source'].reverse())
-    posts['source'].reverse();
+    // posts['source'].reverse();
 
 }
 createNumDisks(4);
 
+
+ $('#button').click(function() {
+    console.log($('#disknum').val());
+})
+
 // example of the weight of the first element in object array
-console.log(posts['source'][0].weight)
-console.log(posts)
+// console.log(posts['source'][0].weight)
+// console.log(posts)
 
 // function checkRow(arr, toAdd) {
 //     if (arr.length > 0) && (arr[0].weight > toAdd.weight) {
