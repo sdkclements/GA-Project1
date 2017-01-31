017328c8f15ab4f08e0e051c832311fc3fd7126a
Samuel Clements <sdkclements@gmail.com>
2017-01-31 15:28:02 -0500

    got the movement going, need to prevent

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # GA-Project1
+<!--  User Stories  -->
+---------------------------
+User Story One: this game can get really messy.
--- js/main.js
+++ js/main.js
@@ -1,17 +1,13 @@
-//
-
-
-
-
+//--------------------------------
 var posts = {
-    source: [],
-    aux: [],
-    dist: []
+        source: [],
+        aux: [],
+        dist: []
 
-}
-// function createBoard(){
-// 	var pole = $(`<div class='pole'> pole One </div>`)
-// 	$('.mainContainer').append
+    }
+    // function createBoard(){
+    // 	var pole = $(`<div class='pole'> pole One </div>`)
+    // 	$('.mainContainer').append
 
 // }
 
@@ -22,42 +18,93 @@ class Disk {
         this.post = post;
         this.position = position;
         this.element = $(`<div class='disk' data-weight=${weight}> Disk: ${weight}</div>`);
-        $('#pol1').append ( this.element );
+        $('#pol1').append(this.element);
         // this.addEventListener('click', function(e) {
         // 	console.log(e)
         // })
 
         // Disks.protoype.handleEvent = function(e) {
-// 	switch (e.type) {
-// 		case 'click': this.click(e);
-// 		//Add other types of event listeners here if i need to
-// 	}
-// }
+        // 	switch (e.type) {
+        // 		case 'click': this.click(e);
+        // 		//Add other types of event listeners here if i need to
+        // 	}
+        // }
     }
 }
 
 function createNumDisks(num) {
 
     for (var i = -1; i = num; i++) {
-    	var disk = new Disk(num, 1, num)
+        var disk = new Disk(num, 1, num)
         posts['source'].push(disk)
         num--
     }
     $('.disk').on('click', function(e) {
-    	console.log(this)
-    	$(this).toggleClass('activeDisk')
-    })
-    //console.log(posts['source'].reverse())
-    // posts['source'].reverse();
+            console.log(this)
+            $(this).toggleClass('activeDisk')
+        })
+        //console.log(posts['source'].reverse())
+        // posts['source'].reverse();
 
 }
-createNumDisks(4);
 
 
- $('#button').click(function() {
-    console.log($('#disknum').val());
+
+$('#submit').click(function(e) {
+    e.preventDefault()
+    num = $('#disknum').val()
+    createNumDisks(num)
 })
 
+/// this checks if the
+$('.pole').click(function() {
+    //console.log($(this).children().length)
+    if ($('.disk').hasClass('activeDisk')) {
+        var mover = $('.activeDisk')
+        mover.appendTo(this)
+        //mover.removeClass('activeDisk')
+
+    }
+})
+
+// $('disk').click(function() {
+//         if ($(this).hasClass(active)
+//         })
+
+
+// }
+
+// if $('.disk').hasClass('activeDisk')
+
+// function movePiece() {
+// 	if ($('.disk').hasClass('activeDisk'))
+// 		var mover = $('.activeDisk')
+//     var movingDiv = $(mover).detatch()
+//     movingDiv.appendTo(this)
+
+//     $('.pole').click(function(){
+
+//     })
+
+//     else{
+//     	console.log('no active disks')
+//     }
+
+//      var movingDisk = $('.activeDisk').detach()
+//     movingDisk.appendTo('#pol2')
+
+// }
+
+// $(document).on('click', '.disc', function() {
+//   if ((!isSelected) && ($(this).css('order') < ($(this).siblings('.disc').css('order')))) {
+//       isSelected = true
+//       $(this).addClass('selected')
+//   }  else {
+//     $('.selected').removeClass('selected')
+//     isSelected = false
+//   }
+// }
+// )
 // example of the weight of the first element in object array
 // console.log(posts['source'][0].weight)
 // console.log(posts)
